baa022296569aed5472f1f626d8e87b33d127f58
Abhay Vishwakarma <hb9868313@gmail.com>
2026-02-23 07:51:21 +0530

    Done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import toast, { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 import PageMeta from "../components/common/PageMeta";
 import { useAuth } from "../context/AuthContext";
 import { loginOfficer } from "../apis/officer";
@@ -45,25 +45,6 @@ export default function LoginPage() {
       <PageMeta
         title="Login | Officer Portal"
         description="Login to your Redressal Officer Portal"
-      />
-      <Toaster
-        position="top-right"
-        containerStyle={{ zIndex: 999999 }}
-        toastOptions={{
-          style: {
-            background: "#1D2939",
-            color: "#fff",
-            border: "1px solid #344054",
-            borderRadius: "12px",
-            fontFamily: "Outfit, sans-serif",
-          },
-          success: {
-            iconTheme: {
-              primary: "#465fff",
-              secondary: "#fff",
-            },
-          },
-        }}
       />
 
       <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-4">
